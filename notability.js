7242c6ec9b2e8e78fa31676618a4760cf2f7a982
dongc1 <dongc_1@yahoo.com>
2025-03-07 10:36:14 -0800

    Update notability.js

--- notability.js
+++ notability.js
@@ -1,27 +1,28 @@
-// 解析服务器返回的 JSON 响应
 var chxm1023 = JSON.parse($response.body);
 
-// 修改订阅信息，确保是有效的高级订阅
-chxm1023.data = {
-  "processAppleReceipt": {
-    "error": 0,
-    "subscription": {
-      "productId": "com.gingerlabs.Notability.premium_subscription",
-      "originalTransactionId": "570001184068302",
-      "tier": "premium",
-      "refundedDate": null,
-      "refundedReason": null,
-      "isInBillingRetryPeriod": false,
-      "expirationDate": "2099-09-09T09:09:09.000Z",
-      "gracePeriodExpiresAt": null,
-      "overDeviceLimit": false,
-      "expirationIntent": null,
-      "__typename": "AppStoreSubscription",
-      "user": null,
-      "status": "active" 
-    },
-    "__typename": "SubscriptionResult"
+chxm1023 = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "570001184068302",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "2099-09-09T09:09:09.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
   }
 };
 
-$done({ body: JSON.stringify(chxm1023) });
+$done({body : JSON.stringify(chxm1023)});
